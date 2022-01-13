4c814825dd32943698e43b33d5f8db160c36546f
rivka99 <rivkafl99@gmail.com>
2022-01-12 19:33:24 -0700

    added a search button and some styling

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          Zip Code App
+          GIF Search App
         </h1>
       </header>
       
       <div className="text-field">
-        <label htmlFor="zipCode"><strong>Gif Keyword:</strong></label>
+        <label htmlFor="zipCode" id="zipCode-lbl"><strong>Gif Keyword:</strong></label>
         <input type="text" id="zipCode" name="zipCode"></input>
+        <button type="button" id="searchButton">Search</button>
       </div>
+      <div className='select-btns'>
       <button type="button" id="trendingButton">Trending</button>
       <button type="button" id="randomButton">Random</button>
+      </div>
       <Search />
     </div>
   );
